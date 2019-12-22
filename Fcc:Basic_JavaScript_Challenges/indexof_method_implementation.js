10dc09fb1e9956899aa9af9f6c415bd5e5ce1b0c
asifmrony <asifmrony@gmail.com>
2019-12-22 11:13:26 +0600

    indexof method implemented

---
+++ Fcc:Basic_JavaScript_Challenges/indexof_method_implementation.js
@@ -0,0 +1,9 @@
+/*To find the positon of a character in a string usually indexof() method. here i simply explained how to works*/
+
+var myBrotherName = "Ashik Mahmud Rasel";
+
+for(var i = 0; i < myBrotherName.length; i++){
+    if(myBrotherName[i] === "R"){
+        console.log(i);
+    }
+}
