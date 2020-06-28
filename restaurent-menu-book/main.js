5f457a9a44a6826432834ec334a62621eb096496
asifmrony <asifmrony@gmail.com>
2020-06-28 12:55:29 +0600

    finished with basic functionality

--- restaurent-menu-book/main.js
+++ restaurent-menu-book/main.js
@@ -1,11 +1,14 @@
 var form = document.querySelector('#addForm');
-
 var itemsList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
 
 //form submit event
 form.addEventListener('submit', addItem);
 // delete event
 itemsList.addEventListener('click', removeItem);
+//filter event
+filter.addEventListener('keyup', filterElement);
 
 
 //addItem 
@@ -40,4 +43,30 @@ function addItem(e) {
 function removeItem(e) {
     e.preventDefault();
 
+    //if press delete button 
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are You Sure?')) {
+            var li = e.target.parentElement;
+            itemsList.removeChild(li);
+        }
+    }
+}
+
+
+//filter element function
+function filterElement(e) {
+    var inputValue = e.target.value.toLowerCase();
+    //grab li element
+    var li = itemsList.getElementsByTagName('li');
+    //convert to arry
+    Array.from(li).forEach(function (item) {
+        //store li elements text to itemname
+        var itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(inputValue) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    })
+    // console.log(li);
 }
