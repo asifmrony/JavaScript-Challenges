c67173fbe39b98c467d6cabe0c21f26c709416fc
asifmrony <asifmrony@gmail.com>
2020-07-21 01:15:30 +0600

    dom exercise 1 challenges on hold

---
+++ DOM Exercises 01/exercise.js
@@ -0,0 +1,55 @@
+/* JavaScript DOM Exercises 01 Tutorial: https://youtu.be/EHF7xBUAmrQ */
+
+/*
+  Exercise 01
+  -----------
+  Highlight all of the words over 8 characters long in the paragraph text (with a yellow background for example)
+*/
+
+var paragraph = document.querySelector('p');
+
+paragraph.innerHTML = paragraph.innerText
+    .split(' ')
+    .map(item => item.length > 8 ? `<span style="background-color: yellow">${item}</span>`: item)
+    .join(' ');
+/*
+  Exercise 02
+  -----------
+  Add a link back to the source of the text after the paragraph tag.
+  (https://forcemipsum.com/)
+*/
+var bodyElement = document.getElementsByTagName('body');
+
+var anchorElement = document.createElement('a');
+anchorElement.href = 'https://forcemipsum.com/';
+anchorElement.innerText = 'Generated text from Forcem Ipsum';
+
+bodyElement[0].appendChild(anchorElement);
+/*
+  Exercise 03
+  -----------
+  Split each new sentence on to a separate line in the paragraph text.
+  A sentence can be assumed to be a string of text terminated with a period (.)
+*/
+
+paragraph.innerHTML = paragraph.innerHTML.split(".").join("</p><p>");
+
+/* 
+  Exercise 04
+  -----------
+  Count the number of words in the paragraph tag and display the count afer the heading.
+  You can assume that all words are separated by one singular whitespace.
+*/
+
+console.log(paragraph);
+var words = paragraph.innerText.split(" ")
+
+var newParagraph = document.createElement('h3');
+newParagraph.innerText = "Total Words Count in the paragraph: " + words.length;
+
+document.body.insertBefore(newParagraph, paragraph);
+/*
+  Exercise 05
+  -----------
+  Replace all question marks (?) with thinking faces (🤔) and exclamation marks (!) with astonished faces (😲) 
+*/
