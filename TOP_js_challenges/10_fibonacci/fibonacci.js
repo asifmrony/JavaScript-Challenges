05e91e807233183bafe9f11dc3d16d566c29d44b
asifmrony <asifmrony@gmail.com>
2022-08-12 02:28:30 +0600

    fibonacci series done

--- TOP_js_challenges/10_fibonacci/fibonacci.js
+++ TOP_js_challenges/10_fibonacci/fibonacci.js
@@ -1,25 +1,31 @@
-const fibonacci = function (param) {
+'use strict'
+
+function fibonacci(param) {
     //An array from where param value will be checked against
     // sample fibonacci Numbers() = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 149];
-    const sekuence = [1, 1, 2, 3, 5, 8, 13];
+    let fibonacciNumber = [];
+    if (param < 0) {
+        return 'OOPS';
+    }
 
     //fill up the 'sekuence' array with fibonacci number sekuence
-    // let sum = 0;
-    // for (let x = 0; x < sekuence.length; x++) {
-    //     for (let y = 1; y < 2; y++) {
-    //         const nextNum = sekuence[x] + sekuence[y];
-    //         // console.log(x, y);
-    //         console.log(nextNum);
-    //     }
-    //     // sum += sekuence[x];
-    //     console.log(sekuence[x]);
-    // }
+    let first = 0;
+    let second = 1;
+    for (let x = 0; x < param; x++) {
+        const temp = first + second;
+        if (!x) {
+            fibonacciNumber.push(second)
+        }
+        fibonacciNumber.push(temp);
+        first = second;
+        second = temp;
+    }
 
-    return sekuence[param - 1];
+    return fibonacciNumber[param - 1];
 };
 
 
 
-console.log(fibonacci(4));
-// // Do not edit below this line
-// export default fibonacci;
+// console.log(fibonacci(4));
+// Do not edit below this line
+module.exports = fibonacci;
