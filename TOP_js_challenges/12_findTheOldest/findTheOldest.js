2c145bee244b816274eb206a6fbac2588200b948
asifmrony <asifmrony@gmail.com>
2022-08-13 02:12:14 +0600

    findTheOldest challenge

---
+++ TOP_js_challenges/12_findTheOldest/findTheOldest.js
@@ -0,0 +1,9 @@
+const findTheOldest = function (peopleArray) {
+    const today = new Date().getFullYear();
+    console.log(today);
+    // const filteredArray = peopleArray.filter()
+};
+
+console.log(findTheOldest(6));
+// Do not edit below this line
+// module.exports = findTheOldest;
