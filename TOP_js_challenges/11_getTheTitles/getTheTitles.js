69e5daa6493babbd0bfd650d7b81c352e6ac8874
asifmrony <asifmrony@gmail.com>
2022-08-13 01:53:58 +0600

    minor changes

--- TOP_js_challenges/11_getTheTitles/getTheTitles.js
+++ TOP_js_challenges/11_getTheTitles/getTheTitles.js
@@ -2,6 +2,5 @@ const getTheTitles = function (param) {
     return param.map(item => item.title)
 };
 
-// console.log(getTheTitles([]))
 // Do not edit below this line
 module.exports = getTheTitles;
