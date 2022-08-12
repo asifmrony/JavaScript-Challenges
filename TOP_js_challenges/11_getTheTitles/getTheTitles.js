c76bca85049c56a50f3b1a10db432f61d73e25b3
asifmrony <asifmrony@gmail.com>
2022-08-13 01:53:09 +0600

    getTheTitles challenge added

--- TOP_js_challenges/11_getTheTitles/getTheTitles.js
+++ TOP_js_challenges/11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function () {
-
+const getTheTitles = function (param) {
+    return param.map(item => item.title)
 };
 
+// console.log(getTheTitles([]))
 // Do not edit below this line
 module.exports = getTheTitles;
