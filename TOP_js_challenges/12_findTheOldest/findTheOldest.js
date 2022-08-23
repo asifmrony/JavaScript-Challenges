3a76eded0986c5e3b8a6b02e2d7a951f941c4dcd
asifmrony <asifmrony@gmail.com>
2022-08-24 02:40:57 +0600

    solution 1 added

--- TOP_js_challenges/12_findTheOldest/findTheOldest.js
+++ TOP_js_challenges/12_findTheOldest/findTheOldest.js
@@ -1,9 +1,31 @@
 const findTheOldest = function (peopleArray) {
-    const today = new Date().getFullYear();
-    console.log(today);
-    // const filteredArray = peopleArray.filter()
+    // const today = new Date().getFullYear();
+    // const oldestMan = peopleArray.filter((people) => people.yearOfDeath - people.yearOfBirth - 40);
+    //mapping over
+    const mapp = peopleArray.map((people) => {
+        return age = people.yearOfDeath - people.yearOfBirth;
+    })
+    const sortByOldest = mapp.sort((a, b) => b - a);
+    const oldestMan = peopleArray.filter((people) => people.yearOfDeath - people.yearOfBirth === sortByOldest[0]);
+    return oldestMan;
 };
 
-console.log(findTheOldest(6));
+// console.log(findTheOldest([
+//     {
+//         name: "Carly",
+//         yearOfBirth: 1942,
+//         yearOfDeath: 1970,
+//     },
+//     {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
+//     },
+//     {
+//         name: "Jane",
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941,
+//     },
+// ]));
 // Do not edit below this line
-// module.exports = findTheOldest;
+module.exports = findTheOldest;
