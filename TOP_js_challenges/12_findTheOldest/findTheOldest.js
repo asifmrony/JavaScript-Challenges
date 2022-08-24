67e03803277b68037dc5072f0fc41c200962f918
asifmrony <asifmrony@gmail.com>
2022-08-25 00:02:58 +0600

    find The Oldest assignment done

--- TOP_js_challenges/12_findTheOldest/findTheOldest.js
+++ TOP_js_challenges/12_findTheOldest/findTheOldest.js
@@ -1,31 +1,18 @@
 const findTheOldest = function (peopleArray) {
-    // const today = new Date().getFullYear();
-    // const oldestMan = peopleArray.filter((people) => people.yearOfDeath - people.yearOfBirth - 40);
-    //mapping over
-    const mapp = peopleArray.map((people) => {
-        return age = people.yearOfDeath - people.yearOfBirth;
+    return peopleArray.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
     })
-    const sortByOldest = mapp.sort((a, b) => b - a);
-    const oldestMan = peopleArray.filter((people) => people.yearOfDeath - people.yearOfBirth === sortByOldest[0]);
-    return oldestMan;
 };
 
-// console.log(findTheOldest([
-//     {
-//         name: "Carly",
-//         yearOfBirth: 1942,
-//         yearOfDeath: 1970,
-//     },
-//     {
-//         name: "Ray",
-//         yearOfBirth: 1962,
-//         yearOfDeath: 2011,
-//     },
-//     {
-//         name: "Jane",
-//         yearOfBirth: 1912,
-//         yearOfDeath: 1941,
-//     },
-// ]));
+const getAge = (birth, death) => {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
+
+
 // Do not edit below this line
 module.exports = findTheOldest;
